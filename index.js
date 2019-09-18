14a2af831f8405e49b80fef2241ff084fdd9d3e2
Jamith Nimantha <jamithnimantha@gmail.com>
2019-09-18 14:01:57 +0530

    FIXED 301 REDIRECT ISSUE

--- index.js
+++ index.js
@@ -9,75 +9,104 @@ const dto = {
     address: "Ganesh Complex, Mau Road, Khandari, Agra - 282010",
     link: "https://www.justdial.com/Agra/Chaudhary-Books-Centre-NEAR-KALI-MANDIR-Sikandra/0562PX562-X562-180725132008-J1M8_BZDET?xid=QWdyYSBTY2hvb2wgQm9vayBEZWFsZXJz"
 }
-
 async function main() {
 
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-
     await crawlPages(page);
 
     await browser.close();
 
 }
 
-async function writeToCsv(data){
+async function writeToCsv(data) {
     let csv = new ObjectToCsv(data);
-
-    await csv.toDisk('./sample.csv');
+    await csv.toDisk('./sample.csv', { append: true });
+    console.log("DATA WRITTEN TO CSV !");
 }
 
 async function crawlPages(page) {
     const infos = [];
 
-    let pageCount = 1;
+    // let pageCount = 1;
 
-    // while (true) {
-    await page.goto('https://www.justdial.com/Agra/School-Book-Dealers/nct-11422690/page-' + pageCount, {
-        waitUntil: 'networkidle0'
-    });
+    for (let pageCount = 1;pageCount>0;pageCount++) {
+        console.log("SCRAPING PAGE " + pageCount);
+        // const page = await browser.newPage();
 
-    links = await scrapeFullPage(page);
-    const info = await scrapeInfo(links, page);
-    // infos.push(info);
-    // console.log(infos);
-    await writeToCsv(info);
-    
-    // }
+        /* 
+        // to disable the redirect of the response
+        await page.setRequestInterception(true);
+        page.on('request', request => {
+            if (request.isNavigationRequest() && request.redirectChain().length !== 0) {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        }); 
+        */
+
+        // await page.waitFor(3000);
+        // setTimeout(() => {
+        // }, 3000)
+        const url = 'https://www.justdial.com/Virudhunagar/School-Book-Dealers/nct-11422690/page-'+pageCount;
+        url.trim();
+        // await page.setDefaultTimeout(60000); 
+        await page.setJavaScriptEnabled(false); // this will disable javascript so the status code 301 will not be redirected
+        await page.goto(url,{waitUntil: 'networkidle0'});
+        console.log(page.url()); // prints the current web page url
+        const length = await page.evaluate("document.getElementsByClassName('cntanr').length"); // find there is any element to scrape
+        if (length == 0) {
+            console.log("NO MORE PAGES TO SCRAPE !!!");
+            // page.close();
+            break;
+        }
+        const title = await page.evaluate("document.getElementsByTagName('title')[0].text"); // get page title
+        const target = parseInt(title.replace(/[^0-9\.]/g, ''), 10);  // get the target 
+        links = await scrapeFullPage(page, target);
+        const info = await scrapeInfo(links, page);
+        // infos.push(info);
+        // console.log(infos);
+        await writeToCsv(info);
+    }
 }
 
-async function scrapeFullPage(page) {
+async function scrapeFullPage(page, target) {
+    await page.setJavaScriptEnabled(true);
+    await page.reload();
     const links = [];
-    const target = 100;
+    // const target = 10;
+    console.log("THIS PAGE HAS " + target + " TARGET LISTS");
     try {
         let targetCount = 1;
         let preHeight;
-        while (targetCount < target) {
+        while (targetCount < 20) {
             targetCount = await page.evaluate("document.getElementsByClassName('cntanr').length");
             preHeight = await page.evaluate("document.body.scrollHeight");
             await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
-            // await page.waitForFunction(
-            //     `document.body.scrollHeight > ${preHeight}`
-            // );
+            await page.waitForFunction(
+                `document.body.scrollHeight > ${preHeight}`
+            );
             // console.log(await page.evaluate("window.getComputedStyle(document.querySelector('.icon-oqp'), ':before').getPropertyValue('content')"));
             await page.waitFor(500);
         }
 
     } catch (error) {
-        console.log(error);
+        console.log("SCROLLING TIMEOUT !!");
     }
 
     const html = await page.evaluate(() => document.body.innerHTML);
-    const $ = await cheerio.load(html);
+    const $ = await cheerio.load(html);  // load the html into cheerio
     $(".cntanr").each((index, element) => {
-        const link = ($(element).attr('data-href'));
+        const link = ($(element).attr('data-href')); // get the link of the dealer shop
         const classNames = [];
         $(element).find('.mobilesv').each((index, el) => {
-            const className = $(el).attr('class').split(/\s+/)[1];
+            const className = $(el).attr('class').split(/\s+/)[1]; // returns all the classnames as list
             classNames.push(className);
         });
         const tel = getTelNumber(classNames);
-        links.push({link,tel});
+        // console.log({link,tel});
+        links.push({ link, tel });
     });
 
     return links;
@@ -137,18 +166,17 @@ function getTelNumber(classNames) {
 async function scrapeInfo(links, page) {
     const infos = [];
     for (i = 0; i < links.length; i++) {
-        await page.goto(links[i].link, {
-            waitUntil: 'networkidle0'
-        });
+        await page.goto(links[i].link, {waitUntil: 'networkidle0'});
         const html = await page.evaluate(() => document.body.innerHTML);
         const $ = await cheerio.load(html);
-        const business = $('span.fn').text();
-        const address = $('#fulladdress > span > span').text();
-        const telephone = links[i].tel;
+        const business = $('span.fn').text(); // extract business name
+        const address = $('#fulladdress > span > span').text(); // extract address
+        const telephone = links[i].tel; //extract telephone
         const link = links[i].link;
 
         infos.push({ business, address, telephone, link });
-        console.log(infos[i]);
+        
+        // console.log(infos[i]);
     }
     return infos;
 }
